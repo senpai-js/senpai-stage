0559540338570f0065503086c7db6c19f5b0509d
jtenner <tenner.joshua@gmail.com>
2018-09-26 11:48:37 -0400

    Add EaseFunc type to exports

--- src/ease/index.ts
+++ src/ease/index.ts
@@ -3,49 +3,51 @@
  */
 import { PI, TAU } from "./consts";
 
-function inverse(inFunc: (ratio: number) => number): (ratio: number) => number {
+export type EaseFunc = (ratio: number) => number;
+
+function inverse(inFunc: EaseFunc): EaseFunc {
   return function outFunc(ratio: number): number {
     return 1 - inFunc(1 - ratio);
   };
 }
 
-function inOut(inFunc: (ratio: number) => number): (ratio: number) => number {
+function inOut(inFunc: EaseFunc): EaseFunc {
   const outFunc = inverse(inFunc);
   return (ratio: number): number => ratio < 0.5
     ? 0.5 * inFunc(ratio * 2)
     : 0.5 + 0.5 * outFunc(2 * ratio - 1);
 }
 
-export const easeLinear: (ratio: number) => number = function linear(ratio: number): number {
+export const easeLinear: EaseFunc = function linear(ratio: number): number {
   return ratio;
 };
 
-export const easeInQuad: (ratio: number) => number = (ratio: number): number => ratio * ratio;
-export const easeOutQuad: (ratio: number) => number = inverse(easeInQuad);
-export const easeInOutQuad: (ratio: number) => number = inOut(easeInQuad);
+export const easeInQuad: EaseFunc = (ratio: number): number => ratio * ratio;
+export const easeOutQuad: EaseFunc = inverse(easeInQuad);
+export const easeInOutQuad: EaseFunc = inOut(easeInQuad);
 
-export const easeInCub: (ratio: number) => number =
+export const easeInCub: EaseFunc =
   (ratio: number): number => ratio * ratio * ratio;
-export const easeOutCub: (ratio: number) => number = inverse(easeInCub);
-export const easeInOutCub: (ratio: number) => number = inOut(easeInCub);
+export const easeOutCub: EaseFunc = inverse(easeInCub);
+export const easeInOutCub: EaseFunc = inOut(easeInCub);
 
-export const easeInQuart: (ratio: number) => number =
+export const easeInQuart: EaseFunc =
   (ratio: number): number => ratio * ratio * ratio * ratio;
-export const easeOutQuart: (ratio: number) => number = inverse(easeInQuart);
-export const easeInOutQuart: (ratio: number) => number = inOut(easeInQuart);
+export const easeOutQuart: EaseFunc = inverse(easeInQuart);
+export const easeInOutQuart: EaseFunc = inOut(easeInQuart);
 
-export const easeInQuint: (ratio: number) => number =
+export const easeInQuint: EaseFunc =
   (ratio: number): number => ratio * ratio * ratio * ratio * ratio;
-export const easeOutQuint: (ratio: number) => number = inverse(easeInQuint);
-export const easeInOutQuint: (ratio: number) => number = inOut(easeInQuint);
+export const easeOutQuint: EaseFunc = inverse(easeInQuint);
+export const easeInOutQuint: EaseFunc = inOut(easeInQuint);
 
-export const easeOutSin: (ratio: number) => number =
+export const easeOutSin: EaseFunc =
   (ratio: number): number => Math.sin(ratio * PI * 0.5);
-export const easeInSin: (ratio: number) => number = inverse(easeOutSin);
-export const easeInOutSin: (ratio: number) => number = inOut(easeInSin);
+export const easeInSin: EaseFunc = inverse(easeOutSin);
+export const easeInOutSin: EaseFunc = inOut(easeInSin);
 
 const p = 0.3;
-export const easeOutElastic: (ratio: number) => number =
+export const easeOutElastic: EaseFunc =
   (ratio: number): number => Math.pow(2, -10 * ratio) * Math.sin((ratio - p / 4) * TAU / p) + 1;
-export const easeInElastic: (ratio: number) => number = inverse(easeOutElastic);
-export const easeInOutElastic: (ratio: number) => number = inOut(easeInElastic);
+export const easeInElastic: EaseFunc = inverse(easeOutElastic);
+export const easeInOutElastic: EaseFunc = inOut(easeInElastic);
