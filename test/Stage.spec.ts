4debb60231cad75486efa8cd3b60d4a686b0286b
RedDwarfian <ebright@trupro.com>
2018-10-01 21:04:09 -0400

    Added PreInterpolate Event test

---
+++ test/Stage.spec.ts
@@ -0,0 +1,31 @@
+import { Cursor, SpriteType } from "../src/util";
+import { IButton } from "../src/view/Button";
+import { ITestSetupTemplate, setup } from "./setupUtil";
+
+describe("Button tests", () => {
+  // location of button
+  const x = 50;
+  const y = 50;
+
+  // create a setup template for the very similar state test cases
+  let stateTests: ITestSetupTemplate;
+
+  // setup before each test
+  beforeEach(() => {
+    stateTests = setup().template
+      .perform(t => t
+        .addButton("button", x, y)
+        .addInteractionPoint("ip"))
+      .placeholder()
+      .perform(t => t
+        .updateStage()
+        .renderStage(),
+      );
+  });
+
+  test("PreInterpolate event fires successfully", () => {
+    const { values } = stateTests.feed(t => t.addStageEventCallback("cb", "preInterpolateEvent")).run();
+    expect(values.callbacks.cb).toBeCalled();
+    expect(values.callbacks.cb.mock.calls[0][0].eventType).toBe("PreInterpolate");
+  });
+});
