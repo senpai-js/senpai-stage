1c542a9776e3b9820d61b51c8dc464652f068fdb
Vegard Ekblad Itland <Vegard.Itland@student.uib.no>
2018-10-06 18:02:42 +0200

    Update InteractionManager tests to use setupUtil (WIP)

--- test/InteractionManager.spec.ts
+++ test/InteractionManager.spec.ts
@@ -4,67 +4,25 @@ import { copy, Identity } from "../src/matrix";
 import { IInteractionPoint } from "../src/util";
 import { Button, IButton } from "../src/view/Button";
 import { IInteractionManager, InteractionManager } from "../src/view/InteractionManager";
-
-/**
- * Helper function: create a new IInteractionManager with a fresh audio context.
- */
-const createInteractionManager = (): IInteractionManager => {
-  const audioContext = new AudioContext();
-  const canvas = document.createElement("canvas");
-  return new InteractionManager({
-    audioContext,
-    canvas,
-    height: 600,
-    width: 800,
-  });
-};
-
-/**
- * Helper function: create a button at 50,50.
- */
-const createButton = (id: string, x: number, y: number): Button => {
-  const buttonPos = copy(Identity).translate(x, y).value;
-
-  // create button
-  const button = new Button({
-    definition: null, // TODO: change?
-    id,
-    position: buttonPos,
-    source: null, // TODO: change?
-  });
-  return button;
-};
-
-/**
- * Helper function: create and add an interaction point to the interaction
- * manager, then return it.
- */
-const addPointToInteractionManager = (im: IInteractionManager): IInteractionPoint => {
-  const ip = im.createInteractionPoint("pointName", "Touch");
-  im.addPoint(ip);
-  return ip;
-};
+import { setup, ITestSetupTemplate } from "./setupUtil";
 
 describe("InteractionManager tests", () => {
   // const variables
   const x = 50;
   const y = 50;
+  
+  let stateTests: ITestSetupTemplate;
 
-  let im: IInteractionManager = null;
-  let button: IButton = null;
 
   // Setup before each test case: create an interaction manager.
   beforeEach(() => {
-    im = createInteractionManager();
-
-    button = createButton("button", x, y);
-    im.addSprite(button);
-  });
-
-  // this is probably not necessary, but added for good measure
-  afterEach(() => {
-    im = null;
-    button = null;
+    stateTests = setup().template
+      .perform(t => t) // TODO change as necessary
+      .placeholder()
+      .perform(t => t
+        .updateStage()
+        .renderStage()
+      );
   });
 
   /**
@@ -72,7 +30,8 @@ describe("InteractionManager tests", () => {
    * point with Touch type.
    */
   test("InteractionManager.createInteractionPoint() creates valid Touch interaction point", () => {
-    const point = im.createInteractionPoint("pointName", "Touch");
+    const { values } = stateTests.feed(t => t.addInteractionPoint("pointName")).run();
+    const point = values.points.pointName as IInteractionPoint;
 
     expect(point.id).toBe("pointName");
     expect(point.type).toBe("Touch");
@@ -80,81 +39,111 @@ describe("InteractionManager tests", () => {
 
   /**
    * Simple test that the interaction manager manages to create an interaction
-   * point with Touch type.
+   * point with Mouse type.
+   * TODO: update setupUtil to work
    */
   test("InteractionManager.createInteractionPoint() creates valid Mouse interaction point", () => {
-    const point = im.createInteractionPoint("pointName", "Mouse");
+
+    const { values } = stateTests.feed(t => t).run();
+    const point = values.stage.createInteractionPoint("pointName", "Mouse");
 
     expect(point.id).toBe("pointName");
     expect(point.type).toBe("Mouse");
   });
 
-  test("When calling InteractionManager.pointMove(), the hover flag for the moved interaction point is set", () => {
-    const ip = addPointToInteractionManager(im);
-
-    im.pointMove(ip, { clientX: x, clientY: y } as MouseEvent | Touch);
-
+  test("When calling InteractionManager.pointMove() over a button, the hover flag for the moved interaction point is set", () => {
+    const { values } = stateTests.feed(t => t
+      .addInteractionPoint("pointName")
+      .addButton("button", x, y)
+      .pointMove("pointName", x, y)).run();
+    const ip = values.points.pointName as IInteractionPoint;
+  
     expect(ip.hover.hover).toBe(true);
+  });
+  
+  test("When calling InteractionManager.pointMove() without a sprite underneath, the hover flag is falsy", () => {
+    const { values } = stateTests.feed(t => t
+      .addInteractionPoint("pointName")
+      .pointMove("pointName", x, y)).run();
+    const ip = values.points.pointName as IInteractionPoint;
+  
+    expect(ip.hover).toBeFalsy();
   });
 
   test("When calling InteractionManager.pointDown(), the button's 'active' flag is true", () => {
-    const ip = addPointToInteractionManager(im);
-
-    im.pointDown(ip, { clientX: x, clientY: y } as MouseEvent | Touch);
+    const { values } = stateTests.feed(t => t
+      .addInteractionPoint("pointName")
+      .addButton("button", x, y)
+      .pointDown("pointName", x, y)).run();
+    const ip = values.points.pointName as IInteractionPoint;
+    const button = values.sprites.button as IButton;
 
     expect(button.active).toBe(true);
   });
 
   test("When calling InteractionManager.pointDown(), the point's 'active' property is the button", () => {
-    const ip = addPointToInteractionManager(im);
-
-    im.pointDown(ip, {clientX: x, clientY: y} as MouseEvent | Touch);
+    const { values } = stateTests.feed(t => t
+      .addInteractionPoint("pointName")
+      .addButton("button", x, y)
+      .pointDown("pointName", x, y)).run();
+    const ip = values.points.pointName as IInteractionPoint;
+    const button = values.sprites.button as IButton;
 
     expect(ip.active).toBe(button);
   });
 
   test("When calling InteractionManager.pointDown(), the button's 'down' flag is true", () => {
-    const ip = addPointToInteractionManager(im);
-
-    im.pointDown(ip, {clientX: x, clientY: y} as MouseEvent | Touch);
+    const { values } = stateTests.feed(t => t
+      .addInteractionPoint("pointName")
+      .addButton("button", x, y)
+      .pointDown("pointName", x, y)).run();
+    const ip = values.points.pointName as IInteractionPoint;
+    const button = values.sprites.button as IButton;
 
     expect(button.down).toBe(true);
   });
 
   test("When calling InteractionManager.pointDown(), the point's 'down' flag is true", () => {
-    const ip = addPointToInteractionManager(im);
-
-    im.pointDown(ip, {clientX: x, clientY: y} as MouseEvent | Touch);
+    const { values } = stateTests.feed(t => t
+      .addInteractionPoint("pointName")
+      .pointDown("pointName", x, y)).run();
+    const ip = values.points.pointName as IInteractionPoint;
 
     expect(ip.down).toBe(true);
   });
 
   test("When calling InteractionManager.pointUp(), the point's 'down' flag is false", () => {
-    const ip = addPointToInteractionManager(im);
-
-    im.pointDown(ip, {clientX: x, clientY: y} as MouseEvent | Touch); // ensure that "down" flag has previously been set
-    im.pointUp(ip, {clientX: x, clientY: y} as MouseEvent | Touch);
-
+    const { values } = stateTests.feed(t => t
+      .addInteractionPoint("pointName")
+      .pointDown("pointName", x, y)
+      .pointUp("pointName", x, y)).run();
+    const ip = values.points.pointName as IInteractionPoint;
+    
     expect(ip.down).toBe(false);
   });
 
   test("When calling InteractionManager.pointUp(), the button's 'active' flag is false", () => {
-    const ip = addPointToInteractionManager(im);
-
-    // ensure that "active" flag has previously been set
-    im.pointDown(ip, {clientX: x, clientY: y} as MouseEvent | Touch);
-    im.pointUp(ip, {clientX: x, clientY: y} as MouseEvent | Touch);
+    const { values } = stateTests.feed(t => t
+      .addInteractionPoint("pointName")
+      .addButton("button", x, y)
+      .pointDown("pointName", x, y)
+      .pointUp("pointName", x, y)).run();
+    const ip = values.points.pointName as IInteractionPoint;
+    const button = values.sprites.button as IButton;
 
     expect(button.active).toBe(false);
   });
 
   test("When calling pointDown(), down button event is fired", () => {
-    const ip = addPointToInteractionManager(im);
-    const callback = jest.fn();
-
-    button.pointDownEvent.once(callback); // emitted first on pointDown
-
-    im.pointDown(ip, { clientX: x, clientY: y } as MouseEvent | Touch);
+    const { values } = stateTests.feed(t => t
+      .addInteractionPoint("pointName")
+      .addButton("button", x, y)
+      .addEventCallback("cb", "pointDownEvent", "button")
+      .pointDown("pointName", x, y)).run();
+    const im = values.stage;
+    const ip = values.points.pointName as IInteractionPoint;
+    const button = values.sprites.button as IButton;
+    const callback = values.callbacks.cb as jest.Mock;
 
     expect(callback).toHaveBeenCalledTimes(1);
     const expected: IPointDownEvent = {
@@ -172,24 +161,28 @@ describe("InteractionManager tests", () => {
   });
 
   test("When repeatedly calling pointDown(), down button event is fired only once", () => {
-    const ip = addPointToInteractionManager(im);
-    const callback = jest.fn();
+    const { values } = stateTests.feed(t => t
+      .addInteractionPoint("pointName")
+      .addButton("button", x, y)
+      .addEventCallback("cb", "pointDownEvent", "button")
+      .pointDown("pointName", x, y)
+      .pointDown("pointName", x, y)).run();
+    const callback = values.callbacks.cb as jest.Mock;
 
-    button.pointDownEvent.listen(callback); // emitted first on pointDown
-
-    im.pointDown(ip, { clientX: x, clientY: y } as MouseEvent | Touch);
-    im.pointDown(ip, { clientX: x, clientY: y } as MouseEvent | Touch);
     expect(callback).toHaveBeenCalledTimes(1);
   });
 
   test("When calling pointDown() and pointUp(), up button event is fired", () => {
-    const ip = addPointToInteractionManager(im);
-    const callback = jest.fn();
-
-    button.pointUpEvent.listen(callback); // emitted first on pointDown
-
-    im.pointDown(ip, {clientX: x, clientY: y} as MouseEvent | Touch);
-    im.pointUp(ip, {clientX: x, clientY: y} as MouseEvent | Touch);
+    const { values } = stateTests.feed(t => t
+      .addInteractionPoint("pointName")
+      .addButton("button", x, y)
+      .addEventCallback("cb", "pointUpEvent", "button")
+      .pointDown("pointName", x, y)
+      .pointUp("pointName", x, y)).run();
+    const im = values.stage;
+    const ip = values.points.pointName as IInteractionPoint;
+    const button = values.sprites.button as IButton;
+    const callback = values.callbacks.cb as jest.Mock;
 
     expect(callback).toHaveBeenCalledTimes(1);
 
@@ -208,13 +201,16 @@ describe("InteractionManager tests", () => {
   });
 
   test("When calling pointDown() and pointUp(), click button event is fired", () => {
-    const ip = addPointToInteractionManager(im);
-    const callback = jest.fn();
-
-    button.pointClickEvent.listen(callback); // emitted first on pointDown
-
-    im.pointDown(ip, { clientX: x, clientY: y } as MouseEvent | Touch);
-    im.pointUp(ip, { clientX: x, clientY: y } as MouseEvent | Touch);
+    const { values } = stateTests.feed(t => t
+      .addInteractionPoint("pointName")
+      .addButton("button", x, y)
+      .addEventCallback("cb", "pointClickEvent", "button")
+      .pointDown("pointName", x, y)
+      .pointUp("pointName", x, y)).run();
+    const im = values.stage;
+    const ip = values.points.pointName as IInteractionPoint;
+    const button = values.sprites.button as IButton;
+    const callback = values.callbacks.cb as jest.Mock;
 
     expect(callback).toHaveBeenCalledTimes(1);
     const expected: IPointClickEvent = {
@@ -232,13 +228,19 @@ describe("InteractionManager tests", () => {
     expect(callback.mock.calls[0][0]).toStrictEqual(expected);
   });
 
+  // TODO: Check if this test does what it's supposed to
   test("When calling pointDown(), firstdown interaction manager event is fired", () => {
-    const ip = addPointToInteractionManager(im);
-    const callback = jest.fn();
-
-    im.pointDownEvent.listen(callback); // emitted first on pointDown
-
-    im.pointDown(ip, { clientX: x, clientY: y } as MouseEvent | Touch);
+    const { values } = stateTests.feed(t => t
+      .addInteractionPoint("pointName")
+      .addButton("button", x, y)
+      // There should be a way to specify "add callback to stage"
+      .addEventCallback("cb", "pointDownEvent", "button")
+      .pointDown("pointName", x, y)).run();
+    const im = values.stage;
+    const ip = values.points.pointName as IInteractionPoint;
+    const button = values.sprites.button as IButton;
+    const callback = values.callbacks.cb as jest.Mock;
+    
     const expected: IPointDownEvent = {
       down: true,
       eventType: "PointDown",
@@ -255,12 +257,17 @@ describe("InteractionManager tests", () => {
   });
 
   test("When calling pointDown() and pointUp(), click interaction manager event is fired", () => {
-    const ip = addPointToInteractionManager(im);
-    const callback = jest.fn();
-
-    im.pointUpEvent.once(callback); // emmitted the moment the point goes up
-    im.pointDown(ip, { clientX: 0, clientY: 0 } as MouseEvent | Touch);
-    im.pointUp(ip, { clientX: 0, clientY: 0 } as MouseEvent | Touch);
+    const { values } = stateTests.feed(t => t
+      .addInteractionPoint("pointName")
+      .addButton("button", x, y)
+      // There should be a way to specify "add callback to stage"
+      .addEventCallback("cb", "pointDownEvent", "button")
+      .pointDown("pointName", x, y)).run();
+    const im = values.stage;
+    const ip = values.points.pointName as IInteractionPoint;
+    const button = values.sprites.button as IButton;
+    const callback = values.callbacks.cb as jest.Mock;
+    
     const expected: IPointUpEvent = {
       down: false,
       eventType: "PointUp",
@@ -277,71 +284,64 @@ describe("InteractionManager tests", () => {
   });
 
   test("When buttons overlap, the 'down' event is captured by the button with the highest z level", () => {
-    const ip = addPointToInteractionManager(im);
-    const callback1 = jest.fn();
-    const callback2 = jest.fn();
-
-    // add a second button
-    const button2 = createButton("button2", x, y);
-    im.addSprite(button2);
+    const { values } = stateTests.feed(t => t
+      .addInteractionPoint("pointName")
+      .addButton("button1", x, y)
+      .addButton("button2", x, y)
+      // TODO: set Z level
+      .addEventCallback("cb1", "pointDownEvent", "button1")
+      .addEventCallback("cb1", "pointDownEvent", "button2")
+      .pointDown("pointName", x, y)).run();
+    const callback1 = values.callbacks.cb1 as jest.Mock;
+    const callback2 = values.callbacks.cb2 as jest.Mock;
 
     // set z indices - since button2 has a higher z index, it should be clicked
     // while the original button should not be
-    button.setZ(0);
-    button2.setZ(1);
-
-    button.pointDownEvent.once(callback1);
-    button2.pointDownEvent.once(callback2);
-
-    im.pointDown(ip, { clientX: x, clientY: y } as MouseEvent | Touch);
+    //button.setZ(0);
+    //button2.setZ(1);
 
     expect(callback1).toHaveBeenCalledTimes(0);
     expect(callback2).toHaveBeenCalledTimes(1);
   });
 
   test("When buttons overlap, the 'up' event is captured by the button with the highest z level", () => {
-    const ip = addPointToInteractionManager(im);
-    const callback1 = jest.fn();
-    const callback2 = jest.fn();
-
-    // add a second button
-    const button2 = createButton("button2", x, y);
-    im.addSprite(button2);
-
+    const { values } = stateTests.feed(t => t
+      .addInteractionPoint("pointName")
+      .addButton("button1", x, y)
+      .addButton("button2", x, y)
+      // TODO: set Z level
+      .addEventCallback("cb1", "pointUpEvent", "button1")
+      .addEventCallback("cb2", "pointUpEvent", "button2")
+      .pointDown("pointName", x, y)
+      .pointUp("pointName", x, y)).run();
+    const callback1 = values.callbacks.cb1 as jest.Mock;
+    const callback2 = values.callbacks.cb2 as jest.Mock;
     // set z indices - since button2 has a higher z index, it should be clicked
     // while the original button should not be
-    button.setZ(0);
-    button2.setZ(1);
-
-    button.pointUpEvent.once(callback1);
-    button2.pointUpEvent.once(callback2);
-
-    im.pointDown(ip, {clientX: x, clientY: y} as MouseEvent | Touch);
-    im.pointUp(ip, {clientX: x, clientY: y} as MouseEvent | Touch);
+    //button.setZ(0);
+    //button2.setZ(1);
 
     expect(callback1).toHaveBeenCalledTimes(0);
     expect(callback2).toHaveBeenCalledTimes(1);
   });
 
   test("When buttons overlap, the 'click' event is captured by the button with the highest z level", () => {
-    const ip = addPointToInteractionManager(im);
-    const callback1 = jest.fn();
-    const callback2 = jest.fn();
-
-    // add a second button
-    const button2 = createButton("button2", x, y);
-    im.addSprite(button2);
-
+    const { values } = stateTests.feed(t => t
+      .addInteractionPoint("pointName")
+      .addButton("button1", x, y)
+      .addButton("button2", x, y)
+      // TODO: set Z level
+      .addEventCallback("cb1", "pointClickEvent", "button1")
+      .addEventCallback("cb2", "pointClickEvent", "button2")
+      .pointDown("pointName", x, y)
+      .pointUp("pointName", x, y)).run();
+    const callback1 = values.callbacks.cb1 as jest.Mock;
+    const callback2 = values.callbacks.cb2 as jest.Mock;
+    
     // set z indices - since button2 has a higher z index, it should be clicked
     // while the original button should not be
-    button.setZ(0);
-    button2.setZ(1);
-
-    button.pointClickEvent.once(callback1);
-    button2.pointClickEvent.once(callback2);
-
-    im.pointDown(ip, { clientX: x, clientY: y } as MouseEvent | Touch);
-    im.pointUp(ip, { clientX: x, clientY: y } as MouseEvent | Touch);
+    //button.setZ(0);
+    //button2.setZ(1);
 
     expect(callback1).toHaveBeenCalledTimes(0);
     expect(callback2).toHaveBeenCalledTimes(1);
