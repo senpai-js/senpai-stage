05348dbcd75cdebe7e8819f585867da1f7d845a0
jtenner <tenner.joshua@gmail.com>
2018-10-09 14:46:41 -0400

    strip sprite point movement assertions from Sprite.spec.ts

--- test/Sprite.spec.ts
+++ test/Sprite.spec.ts
@@ -26,81 +26,9 @@ describe("Sprite tests", () => {
     expect(values.sprites.label.id).toBe("label");
   });
 
-  /**
-   * pointUpEvent fires with correct point values
-   * pointDownEvent fires with correct point values
-   * pointClickEvent fires with correct point values
-   * pointMoveEvent fires with correct point values
-   */
-
-  test("pointDown event should fire.", () => {
+  test("isHovering point returns self", () => {
     const { values } = stateTests
-      .feed(t => t
-        .addEventCallback("cb", "pointDownEvent", "label")
-        .pointDown("ip", x, y))
-      .run();
-
-    const { cb } = values.callbacks;
-    expect(cb).toBeCalled();
-  });
-
-  test("pointUp event should fire.", () => {
-    const { values } = stateTests
-      .feed(t => t
-        .addEventCallback("cb", "pointUpEvent", "label")
-        .pointDown("ip", x, y)
-        .pointUp("ip", x, y),
-      )
-      .run();
-
-    const { cb } = values.callbacks;
-    expect(cb).toBeCalled();
-  });
-
-  test("pointClick event should fire.", () => {
-    const { values } = stateTests
-      .feed(t => t
-        .addEventCallback("cb", "pointClickEvent", "label")
-        .pointDown("ip", x, y)
-        .pointUp("ip", x, y),
-      )
-      .run();
-
-    const { cb } = values.callbacks;
-    expect(cb).toBeCalled();
-  });
-
-  test("pointClick event should not fire if pointUp happens away from sprite.", () => {
-    const { values } = stateTests
-      .feed(t => t
-        .addEventCallback("cb", "pointClickEvent", "label")
-        .pointDown("ip", x, y),
-      )
-      .run();
-
-    const { cb } = values.callbacks;
-    expect(cb).not.toBeCalled();
-  });
-
-  test("pointMove event should fire with correct values", () => {
-    const { values } = stateTests
-      .feed(t => t.addEventCallback("cb", "pointMoveEvent", "label")
-        .setSize("label", 50, 50)
-        .pointMove("ip", x, y)
-        .pointMove("ip", x + 10, y + 10),
-      )
-      .run();
-
-    expect(values.callbacks.cb.mock.calls[0][0].x).toBe(x);
-    expect(values.callbacks.cb.mock.calls[0][0].y).toBe(y);
-    expect(values.callbacks.cb.mock.calls[1][0].x).toBe(x + 10);
-    expect(values.callbacks.cb.mock.calls[1][0].x).toBe(y + 10);
-  });
-
-  test("pointMove event should fire with correct values", () => {
-    const { values } = stateTests
-      .feed(t => t.addEventCallback("cb", "pointMoveEvent", "label")
-        .setSize("label", 50, 50)
+      .feed(t => t.setSize("label", 50, 50)
         .pointMove("ip", x, y),
       )
       .run();
@@ -118,54 +46,6 @@ describe("Sprite tests", () => {
     label.narrowPhase = jest.fn(t => label);
     label.isHovering(values.points.ip, label.lastInterpolated);
     expect(label.narrowPhase).toBeCalled();
-  });
-
-  test("pointDown event should set sprite active property to true", () => {
-    const { values } = stateTests
-      .feed(t => t
-        .setSize("label", 50, 50)
-        .pointDown("ip", x + 10, y + 10),
-      )
-      .run();
-
-    const { label } = values.sprites;
-    expect(label.active).toBeTruthy();
-  });
-
-  test("pointDown event should not set sprite active property to true if it goes down away from the sprite", () => {
-    const { values } = stateTests
-      .feed(t => t
-        .pointDown("ip", x - 10, y - 10),
-      )
-      .run();
-
-    const { label } = values.sprites;
-    expect(label.active).not.toBeTruthy();
-  });
-
-  test("pointDown event should set sprite focused property to true", () => {
-    const { values } = stateTests
-    .feed(t => t
-      .setSize("label", 50, 50)
-      .pointDown("ip", x + 10, y + 10),
-      )
-      .run();
-
-    const { label } = values.sprites;
-    expect(label.focused).toBeTruthy();
-    expect(label.isFocused()).toBe(label);
-  });
-
-  test("pointDown event should not set sprite focused property to true if it goes down away from the sprite", () => {
-    const { values } = stateTests
-      .feed(t => t
-        .pointDown("ip", x - 10, y - 10),
-      )
-      .run();
-
-    const { label } = values.sprites;
-    expect(label.focused).not.toBeTruthy();
-    expect(label.isFocused()).not.toBeTruthy();
   });
 
   test("keyDown event should fire on focused sprite.", () => {
