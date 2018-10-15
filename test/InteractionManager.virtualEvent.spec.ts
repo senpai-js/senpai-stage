253b162c7382a671cbd4feec2ecd6026c30caf82
jtenner <tenner.joshua@gmail.com>
2018-10-15 12:21:43 -0400

    Add virtual events

---
+++ test/InteractionManager.virtualEvent.spec.ts
@@ -0,0 +1,59 @@
+
+import { setup, TestSetup } from "./senpaiTestSetup";
+
+describe("InteractionManager virtual events", () => {
+  let tests: TestSetup;
+  const x = 50;
+  const y = 50;
+  beforeEach(() => {
+    tests = setup();
+  });
+  afterEach(() => {
+    tests.dispose();
+  });
+
+  test("touchstart emits touchStartEvent", () => {
+    const { callbacks } = tests.addStageEventCallback("cb", "touchStartEvent")
+      .dispatchTouchEvent("touchstart", x, y);
+
+    expect(callbacks.cb).toBeCalled();
+  });
+
+  test("touchmove emits touchMoveEvent", () => {
+    const { callbacks } = tests.addStageEventCallback("cb", "touchMoveEvent")
+      .dispatchTouchEvent("touchstart", x, y)
+      .dispatchTouchEvent("touchmove", x, y);
+
+    expect(callbacks.cb).toBeCalled();
+  });
+
+  test("touchend emits touchEndEvent", () => {
+    const { callbacks } = tests.addStageEventCallback("cb", "touchEndEvent")
+      .dispatchTouchEvent("touchstart", x, y)
+      .dispatchTouchEvent("touchend", x, y);
+
+    expect(callbacks.cb).toBeCalled();
+  });
+
+  test("touchcancel emits touchCancelEvent", () => {
+    const { callbacks } = tests.addStageEventCallback("cb", "touchCancelEvent")
+      .dispatchTouchEvent("touchstart", x, y)
+      .dispatchTouchEvent("touchcancel", x, y);
+
+    expect(callbacks.cb).toBeCalled();
+  });
+
+  test("touchstart calls addTouchPoint", () => {
+    const { callbacks } = tests.mockStagePrototypeFunction("cb", "addTouchPoint")
+      .dispatchTouchEvent("touchstart", x, y);
+
+    expect(callbacks.cb).toBeCalled();
+  });
+
+  test("touchstart calls pointMove", () => {
+    const { callbacks } = tests.mockStagePrototypeFunction("cb", "pointMove")
+      .dispatchTouchEvent("touchstart", x, y);
+
+    expect(callbacks.cb).toBeCalled();
+  });
+});
